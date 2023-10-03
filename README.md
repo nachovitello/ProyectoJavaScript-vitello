90fdfe208394a1dd67fab2c35fbedf0febf91107
nachovitello <nachovitello12@gmail.com>
2023-10-02 21:02:58 -0300

    agregado objetos y arrays con una funcion reduce

---
+++ README.md
@@ -0,0 +1,12 @@
+# Comparador de Seguros
+
+Este es un programa simple en JavaScript que permite a los usuarios comparar los precios de varios seguros y encontrar el seguro más barato.
+
+## Uso
+
+1. Abre el archivo `index.html` en tu navegador web.
+2. El programa te solicitará tu nombre. Ingresa tu nombre y haz clic en "Aceptar".
+3. A continuación, se te pedirá ingresar la cantidad de seguros que deseas comparar.
+4. Ingresa la cantidad deseada y haz clic en "Aceptar".
+5. Luego, ingresa el nombre y el precio de cada seguro según las indicaciones.
+6. Una vez que hayas ingresado la información de todos los seguros, el programa mostrará el seguro más barato junto con su nombre y precio.
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 //voy a hacer un simulador para ver que service le tenes que hacer a tu auto
 
-const nombreUsuario = prompt("¿Como es tu nombre?");
+/*const nombreUsuario = prompt("¿Como es tu nombre?");
 
 function saludar(aQuienSaludar) {
     alert ("Bienvenido " + aQuienSaludar + " vamos a ver que seguro te conviene");
@@ -36,4 +36,52 @@ function compararPrecios() {
     alert (`El producto más barato es: ${seguroMasBarato.nombre} con un precio de $${seguroMasBarato.precio}`);
 }
 
+compararPrecios();*/
+
+const nombreUsuario = prompt("¿Cómo es tu nombre?");
+
+function saludar(aQuienSaludar) {
+    alert("Bienvenido " + aQuienSaludar + ", vamos a ver qué seguro te conviene");
+}
+
+saludar(nombreUsuario);
+
+function compararPrecios() {
+    const seguros = [];
+    
+    // Solicitar la cantidad de seguros a comparar
+    const cantidadSeguros = parseInt(prompt("¿Cuántos seguros deseas comparar?"));
+
+    if (isNaN(cantidadSeguros) || cantidadSeguros <= 0) {
+        alert("Por favor, ingrese una cantidad válida de seguros.");
+        return;
+    }
+
+    // Obtener precios de los seguros y crear objetos de seguro
+    for (let i = 1; i <= cantidadSeguros; i++) {
+        const nombreSeguro = prompt(`Ingrese el nombre del Seguro ${i}:`);
+        const precio = parseFloat(prompt(`Ingrese el precio del Seguro ${i}:`));
+
+        if (isNaN(precio)) {
+            console.log(`Por favor, ingrese un precio válido para el Seguro ${i}.`);
+            return;
+        }
+
+        // Crear un objeto de seguro y agregarlo al array de seguros
+        const seguro = { nombre: nombreSeguro, precio };
+        seguros.push(seguro);
+    }
+
+    // Encontrar el seguro más barato
+    let seguroMasBarato = seguros[0];
+    for (let i = 1; i < seguros.length; i++) {
+        if (seguros[i].precio < seguroMasBarato.precio) {
+            seguroMasBarato = seguros[i];
+        }
+    }
+
+    alert(`El seguro más barato es: ${seguroMasBarato.nombre} con un precio de $${seguroMasBarato.precio}`);
+}
+
 compararPrecios();
+
